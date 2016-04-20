c3e35f863bd9bd6379a8d293e8e3fa6de6ed975d
Ksenia <gueletina.ksenia@gmail.com>
2016-04-20 14:06:25 -0400

    added new item creation to actions.rb

--- app/actions.rb
+++ app/actions.rb
@@ -2,12 +2,35 @@
 get '/' do
   erb :index
 end
-# 
+#
 # password_digest, null: false
 # session_token
 
 get '/items/new' do
+  @item = Item.new(
+    title: params[:title],
+    description: params[:description],
+    image_path: params[:image_path],
+    price: params[:price],
+    location: params[:location],
+    user_id: params[:user_id]
+  )
   erb :'items/new'
+end
+
+post '/items/new' do
+  @item = Item.new(
+    title: params[:title],
+    description: params[:description],
+    image_path: params[:image_path],
+    price: params[:price],
+    location: params[:location],
+    user_id: params[:user_id]
+  )
+  if @item.save
+    redirect 'items/show'
+  else
+    redirect 'items/new'
 end
 
 get '/items/show' do
