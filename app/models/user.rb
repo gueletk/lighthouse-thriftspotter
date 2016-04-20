fe42790e7a8fc494a435e586ac6c7f0199b26eda
Ksenia <gueletina.ksenia@gmail.com>
2016-04-20 08:11:57 -0400

    email validation + test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   has_many :items
 
   validates :email, presence: true, uniqueness: true
+  validates_format_of :email, :with => /\A([\w+\-]\.?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
 
   before_validation { self.email.downcase! }
 
--- spec/01_user_spec.rb
+++ spec/01_user_spec.rb
@@ -4,16 +4,29 @@ require 'bcrypt'
 describe User do
   before :each do
     password_salt = BCrypt::Engine.generate_salt
-    @user = User.new(
+    @good_user = User.new(
       name: Faker::Name.name,
       username: Faker::Name.name.downcase,
       password_salt: password_salt,
       password_hash: BCrypt::Engine.hash_secret('password', password_salt),
       email: Faker::Internet.email
     )
+    @bad_user = User.new(
+      password_hash: BCrypt::Engine.hash_secret('otherpassword', password_salt),
+      password_salt: password_salt,
+    )
   end
 
   it "should save successfully with valid user data" do
-    expect(@user.save).to eq(true)
+    expect(@good_user.save).to eq(true)
+  end
+
+  it "should not save successfully with invalid email" do
+    @bad_user.attributes = {
+      name: Faker::Name.name,
+      username: Faker::Name.name.downcase,
+      email: "iamnotanemail"
+    }
+    expect(@bad_user.save).to eq(false)
   end
 end
