066062b626aada6f7e8c1f209f6d4b80fcd8bfdd
kristincheverie <kcheverie@gmail.com>
2016-04-20 18:10:37 +0000

    add pending tests for item model

---
+++ spec/02_item_spec.rb
@@ -0,0 +1,26 @@
+require './spec_helper'
+require 'bcrypt'
+
+describe Item do
+  describe "#title" do
+    it "should have a title" do
+    end
+
+    it "should not be more than 50 characters"
+    end
+  end
+
+  describe "#description" do
+    it "should have a description" do
+    end
+
+    it "should not be more than 300 characters"
+    end
+  end
+
+  describe "#price" do
+    it "should be an integer"
+    end
+  end
+
+end
