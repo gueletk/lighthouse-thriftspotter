d3b4ce80c581746d724c88c09541e9ad72686021
kristincheverie <kcheverie@gmail.com>
2016-04-20 18:38:33 +0000

    Add Item validation tests

--- spec/02_item_spec.rb
+++ spec/02_item_spec.rb
@@ -8,36 +8,55 @@ describe Item do
       description: Faker::Lorem.characters(250),
       location: Faker::Lorem.characters(30),
     )
-
   end
 
   describe "#title" do
     it "should have a title" do
-
+      @item.title = nil
+      expect(@item).to_not be_valid
+      expect(@item.errors[:title]).to include "can't be blank"
     end
 
     it "should not be more than 50 characters" do
+      @item.title = Faker::Lorem.characters(51)
+      expect(@item).to_not be_valid
+      expect(@item.errors[:titles]).to include 'is too long (maximum is 50 characters)'
     end
   end
 
   describe "#description" do
     it "should have a description" do
+      @item.description = nil
+      expect(@item).to_not be_valid
+      expect(@item.errors[:description]).to include "can't be blank"   
     end
 
     it "should not be more than 300 characters" do
+      @item.description = Faker::Lorem.characters(301)
+      expect(@item).to_not be_valid
+      expect(@item.errors[:description]).to include 'is too long (maximum is 300 characters)'
     end
   end
 
   describe "#price" do
-    it "should be an integer" do
-    end
+   it 'must be a number (integer) only' do
+     @item.price = 'abc'
+     expect(@item).to_not be_valid
+     expect(@item.errors[:price]).to include 'is not a number'
+   end
   end
 
   describe "#location" do
     it "should have a location" do
+      @item.location = nil
+      expect(@item).to_not be_valid
+      expect(@item.errors[:location]).to include "can't be blank"     
     end
 
     it "should not be more than 50 characters" do
+      @item.location = Faker::Lorem.characters(51)
+      expect(@item).to_not be_valid
+      expect(@item.errors[:location]).to include 'is too long (maximum is 50 characters)'      
     end
   end
 
