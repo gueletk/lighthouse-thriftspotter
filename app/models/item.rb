0ec1f5c359d64847b904d5ff7c86190f01da28a5
Ksenia <gueletina.ksenia@gmail.com>
2016-04-22 21:26:29 -0400

    cleaned up code

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,14 @@
-require 'pry'
 class Item < ActiveRecord::Base
 
   belongs_to :user
   has_many :likes, :dependent => :delete_all
   has_many :comments, :dependent => :delete_all
 
-  validates_presence_of :title, :description, :location, :image_path
-  #validate presence of user once login functionality is set
+  validates_presence_of :title, :description, :location, :image_path, :user
 
-  validates :title, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
+  validates :title, :location,
+    length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
   validates :description, length: { maximum: 300, too_long: "%{count} characters is the maximum allowed" }
-  validates :location, length: { maximum: 50, too_long: "%{count} characters is the maximum allowed" }
   validates :price, numericality: { only_integer: true, greater_than: 0, :allow_blank => true }
 
   def num_likes
