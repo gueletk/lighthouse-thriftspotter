eabeda191d6fa8d0906c54d2fddf688831f6c1e7
Ksenia <gueletina.ksenia@gmail.com>
2016-05-09 17:38:16 -0400

    change db setup for heroku

--- config/database.rb
+++ config/database.rb
@@ -10,7 +10,10 @@ configure do
       database: "db/db.sqlite3"
     }
   else
-    set :database, ENV['DATABASE_URL']
+    set :database, {
+    adapter: "postgres"
+    database: ENV['DATABASE_URL']
+  }
   end
 
   # Load all models from app/models, using autoload instead of require
